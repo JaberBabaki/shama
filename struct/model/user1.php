5420ea383199c03ffaed41bdfcad057571fbdb2e
Ali Zamani <zamaniali1995@gmail.com>
2020-02-01 20:26:43 +0330

    The cancelAppointmentByUser function for cancelling an appointment in database was added

--- struct/model/user1.php
+++ struct/model/user1.php
@@ -10,4 +10,24 @@ class User1Model {
     $records = $db->query("SELECT * FROM s_counseling_center WHERE counselingName LIKE '%id%'");
     return $records;
   }
+  
+  public static function cancelAppointmentByUser($calendar_id, $cardNum, $name){
+    $db = Db::getInstance();
+    $db->insert("
+                UPDATE 
+                  s_calender_psych
+                SET 
+                  appointment = 0,
+                  user_id = NULL
+                WHERE
+                  calender_id=$calendar_id
+      ");
+      
+    $db->insert("
+                INSERT INTO
+                  s_cancel_appointment
+                (calendar_id, cardNum, name)VALUES($calendar_id, $cardNum, '$name')
+              ");
+
+  }
 }
