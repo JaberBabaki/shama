1bcd3f4edcecc35a95c1714f5b121b980dc84049
Ali Zamani <zamaniali1995@gmail.com>
2020-02-06 01:34:08 +0330

    The Appointment class was added to the algorithm of user 1 in order to calculate the first available booked appointment, the available appointments and canceled appointments

---
+++ struct/controller/algorithms/user1.php
@@ -0,0 +1,99 @@
+<?php
+class Appointment{
+
+    function __construct($user_id){
+        $this->user_id = $user_id;
+        $this->record = User1Model::getBookedAppointmentByUserId($user_id);  
+        // $date = 0;
+        // $dateId = -1;
+        // if($this->recordPsych!=null){
+        //     for ($i=0; $i<count($this->recordPsych); $i++){
+        //         if ($this->recordPsych[$i]['date']==$date){
+        //             $this->recordPsychList[$dateId][]= $this->recordPsych[$i];
+        //             //($this->recordPsychList[$dateId], $this->recordPsych[$i]);
+        //         }else{
+        //             $dateId++;
+        //             $date = $this->recordPsych[$i]['date'];
+        //             $this->recordPsychList[$dateId][] = $this->recordPsych[$i]; 
+        //         }     
+        //     }
+        // }
+            
+    }
+
+    function getAllAvailable(){
+        if($this->record==null){
+            return null;
+        }else{
+            return $this->record;
+        }
+    }   
+
+    function getFirstAvailable(){
+        if ($this->record==null){
+            return null;
+        }else{
+            return $this->record[0];
+        }
+    }     
+    
+    function getCanceled(){
+        $canceledAppointment = User1Model::getCanceledAppointmentByUserId($this->user_id); 
+        if ($canceledAppointment==null){
+            return null;
+        }else{
+            return $canceledAppointment;
+        }
+    }    
+
+    // function secondAvailable(){
+    //     if (!array_key_exists(1, $this->recordPsychList)){
+    //         return null;
+    //     }else{
+    //         $date = new DateTime($this->recordPsychList[1][0]['date']);
+    //         $diff = $date->diff($this->now);
+    //         $resultArray = array();
+    //         switch ($diff->days){
+    //             case 0:
+    //                 if($diff->invert==0){
+    //                     array_push($resultArray, 'امروز');
+    //                     break;
+    //                 }else{
+    //                     array_push($resultArray, 'فردا');
+    //                     break;
+    //                 }
+                    
+    //             case 1:
+    //                 array_push($resultArray, 'فردا');
+    //                 break;
+    //             case 2:
+    //                 array_push($resultArray, 'پس فردا');
+    //                 break;
+    //             default:
+    //                 array_push($resultArray, stringConverter($diff->days, 'enToFa').' روز دیگر ');
+    //                 break;
+    //         }
+    //         for($i=0; $i<count($this->recordPsychList[1]); $i++){
+    //             if($i==5)break;
+    //             $date = new DateTime($this->recordPsychList[1][$i]['date'].$this->recordPsychList[1][$i]['startTime']);
+    //             array_push(
+    //                 $resultArray,
+    //                 $this->pmAm($date->format('H:i')).''.stringConverter($date->format('H:i'), 'enToFa')
+    //             );
+    //         }
+    //     }
+    //     return $resultArray;
+    // }
+
+    // function pmAm($date){
+    //     if ($date < '12:00' && $date >= '8:00')return 'صبح';
+    //     elseif ($date < '15:00' && $date >= '12:00')return ' ظهر ';
+    //     elseif ($date < '18:00' && $date >= '15:00')return ' بعدازظهر ';
+    //     elseif ($date < '20:00' && $date >= '18:00')return ' عصر ';
+    //     elseif ($date < '24:00' && $date >= '20:00')return ' شب ';
+    //     elseif ($date < '8:00' && $date >= '00:00')return ' صبح ';
+    //     }
+      
+
+}
+?>
