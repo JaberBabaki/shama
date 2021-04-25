d4aa6cbe8ecd456983e260a2d55b4e7e98e41202
Jaber Babaki <jaber.babaki94@gmail.com>
2021-04-25 17:06:15 +0430

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 global $config;
 $config['db']['servername']='localhost';
-$config['db']['username']='devmod_shama';
-$config['db']['$password']='sTEk4cY2';
-$config['db']['dbname']='devmod_shama';
+$config['db']['username']='';
+$config['db']['$password']='';
+$config['db']['dbname']='';
 
 $config['lang']='fa';
-$config['salt']='sdsdfdgdrtregFFLGOSJOFk4r9fkDKg';
+$config['salt']='';
 
-$config['url1']="Location:http://shama.local";
-$config['url2']="http://shama.local";
+$config['url1']="";
+$config['url2']="";
 
-// $config['url1']="Location:http://devmodat.ir";
-// $config['url2']="http://devmodat.ir";
+// $config['url1']="";
+// $config['url2']="";
 //jaber
 //ali
 //ali branch feature
